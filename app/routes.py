782c93395fc73b3598ef14fdc0e9b78c723e3f4e
Eric Frankel <eric-frankel@cox.net>
2018-07-18 03:41:01 -0700

    finally finished map rendering

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 from app.forms import RunForm
 
 @app.route('/index')
@@ -7,7 +7,13 @@ from app.forms import RunForm
 def index():
     return render_template('index.html')
 
-@app.route('/input')
+@app.route('/input',methods=['GET','POST'])
 def input():
     form = RunForm()
-    return render_template('input.html')
+    if form.validate_on_submit():
+        return render_template('map.html', distance=form.distance.data, pace=form.pace.data)
+    return render_template('input.html', form=form)
+
+@app.route('/map')
+def map(distance, pace):
+    return render_template('map.html',distance=distance, pace=pace)
