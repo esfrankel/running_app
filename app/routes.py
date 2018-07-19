3a41fcf262a0b7e052513f744af42db7195d4403
Eric Frankel <eric-frankel@cox.net>
2018-07-19 01:04:05 -0700

    generated lats/longs work now

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
-from flask import render_template, redirect
+from flask import render_template
 from app.forms import RunForm
 from app.scripts import find_coords, triangle_point
-import requests
-import json
+import requests, random
+import googlemaps
 
 @app.route('/index')
 @app.route('/')
@@ -14,15 +14,19 @@ def index():
 def input():
     form = RunForm()
     if form.validate_on_submit():
-        print([form.latitude.data, form.longitude.data])
-        pair = find_coords(form.latitude.data, form.longitude.data, form.distance.data * 0.3)
-        print(pair)
-        r = requests.get("http://api.geonames.org/findNearestIntersectionOSMJSON?lat="+str(round(pair[0],3))+"&lng="+str(round(pair[1],2))+"&username=efrankel")
+        theta = random.uniform(0.0,360.0)
+        pair = find_coords(form.latitude.data, form.longitude.data, form.distance.data * 0.3, theta)
+        r = requests.get("http://api.geonames.org/findNearestIntersectionOSMJSON?lat="+str(pair[0])+"&lng="+str(pair[1])+"&username=efrankel")
         generated_loc = [float(r.json()['intersection']['lat']),float(r.json()['intersection']['lng'])]
-        print(generated_loc)
-        third_point = triangle_point([form.latitude.data, form.longitude.data], generated_loc, form.distance.data * 0.4)
-        print(third_point)
-        return [[form.latitude.data, form.longitude.data], generated_loc, third_point]
+        third_point = triangle_point([form.latitude.data, form.longitude.data], generated_loc, form.distance.data * 0.4, theta)
+        gmaps = googlemaps.Client(key='AIzaSyBEgWq5G822EXJIgfviFqJRf7vVE6_F5Lc')
+        loc1 = gmaps.reverse_geocode((form.latitude.data, form.longitude.data))
+        loc2 = gmaps.reverse_geocode((generated_loc[0], generated_loc[1]))
+        loc3 = gmaps.reverse_geocode((third_point[0], third_point[1]))
+        dir1 = gmaps.directions(loc1, loc2, mode="walking")
+        dir2 = gmaps.directions(loc2, loc3, mode="walking")
+        dir3 = gmaps.directions(loc3, loc1, mode="walking")
+        return
     return render_template('input.html', form=form)
 
 @app.route('/map')
